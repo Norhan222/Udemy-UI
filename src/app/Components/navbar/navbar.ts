9a8d3047e4256126c6a2da0d864dc22be1840b09
Norhan222 <norhanallam01@gmail.com>
2025-12-14 22:38:07 +0200

    bootsrap

--- src/app/Components/navbar/navbar.ts
+++ src/app/Components/navbar/navbar.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../Services/category-service';
 import { Category } from '../../Models/category';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatListModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar implements OnInit{
   categories!:Category[];
+
  constructor(private catService:CategoryService) {}
   ngOnInit(): void {
     this.catService.getCategories().subscribe((data)=>{
       console.log(data);
     });
   }
-
 }
+
+
