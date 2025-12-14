ff8b5c56374a1fa9ab271171e16153e91782063a
Nedaa Gaafer <nedaagaafer2000@gmail.com>
2025-12-14 22:44:10 +0200

    add bootsrap library

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './Components/navbar/navbar';
+import { TopicsCarousel } from './Components/topics-carousel/topics-carousel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
+  imports: [RouterOutlet,Navbar,TopicsCarousel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
