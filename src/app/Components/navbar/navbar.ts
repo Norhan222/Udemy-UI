e142c442dc621c46d811b3b9f63eb42f52c1dedd
Norhan222 <norhanallam01@gmail.com>
2025-12-16 00:21:04 +0200

    addMenu

--- src/app/Components/navbar/navbar.ts
+++ src/app/Components/navbar/navbar.ts
@@ -7,12 +7,10 @@ import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
 
+
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatListModule, RouterLink],
+  imports: [CommonModule,RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
@@ -24,6 +22,32 @@ export class Navbar implements OnInit{
     // this.catService.getCategories().subscribe((data)=>{
     //   console.log(data);
     // });
+  }
+
+ isOpen = false;
+  activeMenu: string | null = null;
+
+  subMenus: any = {
+    ai: ['AI Fundamentals', 'ChatGPT', 'Machine Learning'],
+    career: ['Web Developer', 'Data Analyst', 'UX Designer'],
+    cert: ['AWS', 'Azure', 'Google Cloud'],
+    dev: ['Web Development', 'Mobile Apps', 'Game Development'],
+    business: ['Entrepreneurship', 'Management'],
+    finance: ['Accounting', 'Investing'],
+    it: ['Networking', 'Cyber Security']
+  };
+
+  openMenu() {
+    this.isOpen = true;
+  }
+
+  closeMenu() {
+    this.isOpen = false;
+    this.activeMenu = null;
+  }
+
+  setActive(menu: string) {
+    this.activeMenu = menu;
   }
 }
 
