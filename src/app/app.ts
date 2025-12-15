051cab433e7a616247c0335ff681598ad1455c07
Nedaa Gaafer <nedaagaafer2000@gmail.com>
2025-12-16 00:41:51 +0200

    add new changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './Components/navbar/navbar';
 import { Footer } from './Components/footer/footer';
+import { Header } from './Components/header/header';
+import { StartYourCourses } from './Components/start-your-courses/start-your-courses';
+import { RecommenedCourses } from './Components/recommened-courses/recommened-courses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer],
+  imports: [RouterOutlet,Navbar,Header,StartYourCourses,RecommenedCourses,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
