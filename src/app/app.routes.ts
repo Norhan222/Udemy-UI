06d51909bd8382166a8d3d787179933842a20d25
khaled mahmoud <khaledamalb22@gmail.com>
2025-12-21 12:48:06 -0800

    add Notfound component  and hendel app-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './Components/login/login';
 import { Register } from './Components/register/register';
+import { Notfound } from './Components/notfound/notfound';
+import { StartYourCourses } from './Components/start-your-courses/start-your-courses';
 
 export const routes: Routes = [
-    //  {path:'', redirectTo: 'parent', pathMatch: 'full'}
+    {path:'', redirectTo: 'StartYourCourses', pathMatch: 'full'},
+    {path:'StartYourCourses', component:StartYourCourses,title:'StartYourCourses'},
     {path:'Login', component:Login,title:'Login'},
     {path:'Register', component:Register,title:'Register'},
-    // ,{path:'**', component:,title:'Not Found Page'}
+    {path:'**', component:Notfound,title:'Not Found Page'}
 ];
