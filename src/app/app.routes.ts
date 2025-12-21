d053678a029c67059d938157e5938425a330a048
khaled mahmoud <khaledamalb22@gmail.com>
2025-12-21 13:15:41 -0800

    edit app-route & add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './Components/login/login';
 import { Register } from './Components/register/register';
 import { Notfound } from './Components/notfound/notfound';
-import { StartYourCourses } from './Components/start-your-courses/start-your-courses';
+import { Home } from './Components/home/home';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'StartYourCourses', pathMatch: 'full'},
-    {path:'StartYourCourses', component:StartYourCourses,title:'StartYourCourses'},
+    {path:'', redirectTo: 'Home', pathMatch: 'full'},
+    {path:'Home', component:Home,title:'Home'},
     {path:'Login', component:Login,title:'Login'},
     {path:'Register', component:Register,title:'Register'},
     {path:'**', component:Notfound,title:'Not Found Page'}
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './Components/NavbarComponents/navbar/navbar';
 import { Footer } from './Components/footer/footer';
-import { Header } from './Components/header/header';
-import { StartYourCourses } from './Components/start-your-courses/start-your-courses';
-import { RecommenedCourses } from './Components/recommened-courses/recommened-courses';
-import { TopicsRecommended } from './Components/topics-recommended/topics-recommended';
-import { TrendingCourses } from './Components/trending-courses/trending-courses';
-import { FeaturedCourses } from './Components/featured-courses/featured-courses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Header,StartYourCourses,RecommenedCourses,TopicsRecommended,TrendingCourses,FeaturedCourses,Footer],
+  imports: [RouterOutlet,Navbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
