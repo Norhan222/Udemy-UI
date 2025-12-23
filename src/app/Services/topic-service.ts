8327cb97be8c9d9808411363b35e9ba74a2f8eef
Nedaa Gaafer <nedaagaafer2000@gmail.com>
2025-12-23 05:18:48 +0200

    add component home befor signup

--- src/app/Services/topic-service.ts
+++ src/app/Services/topic-service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Topic } from '../Models/topic';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TopicService {
+  baseUrl = environment.apiUrl;
+  constructor(private http:HttpClient) {}
+  getTopics():Observable<Topic[]> {
+      return this.http.get<Topic[]>(`${this.baseUrl}/Topic?page=1&pageSize=10`);
+    }
   
 }
