9d8ae4a8af0401da7c38bd523197127b42b6ac71
Norhan222 <norhanallam01@gmail.com>
2025-12-24 20:18:09 +0200

    edit  route

--- src/app/Components/Dashboard/Performance/performance-layout/performance-layout.ts
+++ src/app/Components/Dashboard/Performance/performance-layout/performance-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Overview } from '../overview/overview';
 import { Reviews } from '../reviews/reviews';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-performance-layout',
-  imports: [Overview,Reviews],
+  imports: [Overview,Reviews,RouterOutlet],
   templateUrl: './performance-layout.html',
   styleUrl: './performance-layout.css',
 })
