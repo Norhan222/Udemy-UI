037c2021131421dc4c35262babebebd3dd37c916
MahmoudShabanOmar <mshomar222000@gmail.com>
2026-01-03 22:24:33 +0200

    https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 //  apiUrl: 'https://localhost:7009/api',
-// apiUrl: 'http://udemyfinalproject.runasp.net/api',
-  apiUrl: 'http://udemyfinalproject.runasp.net/swagger',
+apiUrl: 'https://udemyfinalproject.runasp.net/api',
+//   apiUrl: 'http://udemyfinalproject.runasp.net/swagger',
 
 
     production: false
